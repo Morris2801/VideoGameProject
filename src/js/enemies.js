d75a29655796f506070b74f15d899dcfc329f89c
Mauricio Emilio Monroy González <mauriciomonroyg@hotmail.com>
2025-03-19 11:45:41 -0600

    Begun defining enemy classes. Missing clarification regarding attacks, movement frames. In levels.js and world.js, missing clarification regarding how the objClass should be handled

--- src/js/enemies.js
+++ src/js/enemies.js
@@ -1,15 +1,82 @@
-// Enemies
+// BaseEnemies
 class BaseEnemy extends BaseCharacter{
     constructor(_color, width, height, x, y, _type) {
         super("red", width, height, x, y, _type);
-        //Atributes
     }
 }
+class Mariachi extends BaseEnemy {
+    constructor(_color, width, heigth, x, y, _type){
+        super("red", width, heigth,x,y,"mariachi");
+        this.health = 10;
+        this.damage = 1;
+        this.setMovementFrames('right',  [], [],);
+        this.setMovementFrames('left', [], []);
+        this.setMovementFrames('up', [], []);
+        this.setMovementFrames('down', [], []);
+    }
+}
+class Tlaxcalteca extends BaseEnemy{
+    constructor(_color, width, heigth, x, y, _type){
+        super("red", width, heigth,x,y,"tlaxcalteca");
+        this.health = 15;
+        this.damage = 2;
+        this.setMovementFrames('right',  [], [],);
+        this.setMovementFrames('left', [], []);
+        this.setMovementFrames('up', [], []);
+        this.setMovementFrames('down', [], []);
+    }
+}
+class MayanWarrior extends BaseEnemy{
+    constructor(_color, width, heigth, x, y, _type){
+        super("red", width, heigth,x,y,"mayanWarrior");
+        this.health = 20;
+        this.damage = 3;
+        this.setMovementFrames('right',  [], [],);
+        this.setMovementFrames('left', [], []);
+        this.setMovementFrames('up', [], []);
+        this.setMovementFrames('down', [], []);
+    }
+}
+class Devil extends BaseEnemy{
+    constructor(_color, width, heigth, x, y, _type){
+        super("red", width, heigth,x,y,"devil");
+        this.health = 35;
+        this.damage = 4;
+        this.setMovementFrames('right',  [], [],);
+        this.setMovementFrames('left', [], []);
+        this.setMovementFrames('up', [], []);
+        this.setMovementFrames('down', [], []);
+    }
+}
+
+// Bosses
 class BaseBoss extends BaseCharacter{
     constructor(_color, width, height, x, y, _type) {
         super("red", width, height, x, y, _type);
         //Atributes
         this.attack1 = 0;
         this.attack2 = 0;
+    }
+}
+class Quetzalcoatl extends BaseBoss{
+    constructor(_color, width, height, x, y, _type){
+        super("red", width, height, x, y, "quetzalcoatl");
+        this.health = 85;
+        this.damage = Math.floor(Math.random() * 6-4) + 4;
+        this.setMovementFrames('right',  [], [],);
+        this.setMovementFrames('left', [], []);
+        this.setMovementFrames('up', [], []);
+        this.setMovementFrames('down', [], []);
+    }
+}
+class AhPuch extends BaseBoss{
+    constructor(_color, width, height, x, y, _type){
+        super("red", width, height, x, y, "ahPuch");
+        this.health = 110;
+        this.damage = Math.floor(Math.random() * 7-5) + 5;
+        this.setMovementFrames('right',  [], [],);
+        this.setMovementFrames('left', [], []);
+        this.setMovementFrames('up', [], []);
+        this.setMovementFrames('down', [], []);
     }
 }
